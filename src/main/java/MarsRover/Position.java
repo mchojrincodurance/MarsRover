4274d1b567e57609c1c1478f59b974aa8c7034db
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-07-01 16:02:23 +0200

    Converting Position back to a record (As per IDE suggestion)

--- src/main/java/MarsRover/Position.java
+++ src/main/java/MarsRover/Position.java
@@ -1,29 +1,12 @@
 package MarsRover;
 
-public final class Position {
-    private final int x;
-    private final int y;
+public record Position(int x, int y) {
 
-    public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int x() {
-        return x;
-    }
-
-    public int y() {
-        return y;
-    }
-
-    public String toString()
-    {
+    public String toString() {
         return x + ":" + y;
     }
 
-    public boolean equals(Position otherPosition)
-    {
+    public boolean equals(Position otherPosition) {
         return this.x == otherPosition.x && this.y == otherPosition.y;
     }
 }
